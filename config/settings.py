d208026294f137a5a08e6d76f4713c5706eed416
Rutuja Sabale <157_2023_cse@yes.edu.in>
2026-08-19 20:26:25 +0530

    Fix static files for deployment

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
 
